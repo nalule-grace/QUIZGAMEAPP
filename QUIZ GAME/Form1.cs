f3d621c70c9edae9cd1c58084a5ddd2e77a76531
nalule-grace <g.nalule@alustudent.com>
2024-07-27 11:26:47 +0200

    quizzes

---
+++ QUIZ GAME/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUIZ_GAME
+{
+    public partial class Form1 : Form
+    {
+
+        //quiz game variables
+
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+
+            totalQuestions = quizQuestions.Count;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// handles the event when answer button is clicked
+        /// </summary>
+        /// <param name="sender">source of event</param>
+        /// <param name="e">event data</param>
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            try
+            {
+                var senderObject = (Button)sender;
+
+                int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+                if (buttonTag == correctAnswer)
+                {
+                    score += quizQuestions[questionNumber -1].Weight;
+                    MessageBox.Show("correct!");
+                }
+                else if (buttonTag != correctAnswer)
+                {
+                    MessageBox.Show(
+                        "Wrong choice"
+                        );
+                }
+
+                if (questionNumber == totalQuestions)
+                {
+                    //score percentage
+
+
+                   // percentage = (int)Math.Round((double)(score * 100) / totalQuestions * quizQuestions[0].Weight);
+                   percentage = (score/17) * 100;
+
+
+                    MessageBox.Show(
+                        "QUIZ ENDED!!!" + Environment.NewLine +
+                        "You have answered " + score + " questions correctly" + Environment.NewLine +
+                        "Your score is" + score + "/" + "17" + Environment.NewLine +
+                        "Your total percentage is " + percentage + "%" + Environment.NewLine +
+                        "click OK to play again"
+
+                        );
+
+
+                    score = 0;
+                    questionNumber = 0;
+                    // askQuestion(questionNumber);
+                }
+
+                questionNumber++;
+                askQuestion(questionNumber);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"An error occured: {ex.Message}", "error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+            }
+
+        }
+
+        /// <summary>
+        /// shows quiz questions
+        /// </summary>
+        public class QuizQuestion
+        {
+            public Image Image { get; set; }
+            public string QuestionText { get; set; }
+            public List<string> Answers { get; set; }
+            public int correctAnswer { get; set; }
+            public int Weight { get; set; }
+
+        }
+
+        //initializing question
+        List<QuizQuestion> quizQuestions = new List<QuizQuestion>
+        {
+            new QuizQuestion
+            {
+                Image = Properties.Resources.quiz,
+                QuestionText = "Which studio produces the Avengers?",
+                Answers = new List<string> { "cartoon network", "DC", "Marvel", "ALU" },
+                correctAnswer = 3,
+                Weight = 2
+            },
+            new QuizQuestion
+            {
+
+                Image = Properties.Resources.ironman,
+                QuestionText = "What is the name of the main character from IronMan?",
+                Answers = new List<string> { "Bang olufsen", "Tony Stark", "The grinch", "Itachi" },
+                correctAnswer = 2,
+                Weight=1
+            },
+            new QuizQuestion
+            {
+
+                Image = Properties.Resources.avengers1,
+                QuestionText = "How much do the avengers pay if they destroy your car?",
+                Answers = new List<string> { "1 million", "2 infinity stones", "10$", "😂😂😂😂😂" },
+                correctAnswer = 4,
+                Weight = 2
+            },
+            new QuizQuestion
+            {
+
+                Image = Properties.Resources.spider_man,
+                QuestionText = "Which city does spiderman protect from crime?",
+                Answers = new List<string> { "kigali", "Queens", "Newyork", "Canon event" },
+                correctAnswer = 2,
+                Weight = 2
+            },
+            new QuizQuestion
+            {
+
+                Image = Properties.Resources.hulk,
+                QuestionText = "What does the hulk do for a job?",
+                Answers = new List<string> { "street dancer", "genetic researcher", "actor", "bio technology" },
+                correctAnswer = 2,
+                Weight = 3
+            },
+            new QuizQuestion
+            {
+
+                Image = Properties.Resources.thanos,
+                QuestionText = "What did thanos believe in?",
+                Answers = new List<string> { "world peace", "Jesus Christ", "Universal balance", "Almighty" },
+                correctAnswer = 3,
+                Weight = 2
+            },
+            new QuizQuestion
+            {
+
+                Image = Properties.Resources.captain_america,
+                QuestionText = "What is another name for captain America?",
+                Answers = new List<string> { "The goat", "cap", "captain man", "winter's soilder" },
+                correctAnswer = 2,
+                Weight = 1
+            },
+            new QuizQuestion
+            {
+
+                Image = Properties.Resources.avengers2,
+                QuestionText = "Which avengers installation has this posterMan?",
+                Answers = new List<string> { "infinity war", "Age of ultron", "endgame", "dawn of justice" },
+                correctAnswer = 3,
+                Weight = 1
+            },
+            new QuizQuestion
+            {
+
+                Image = Properties.Resources.quiz,
+                QuestionText = "Which letter is outside of Avengers HQ?",
+                Answers = new List<string> { "A", "B", "HQ", "M" },
+                correctAnswer = 1,
+                Weight = 1
+            },
+            new QuizQuestion
+            {
+
+                Image = Properties.Resources.quiz,
+                QuestionText = "The avengers are...?",
+                Answers = new List<string> { "Villains", "Antagonists", "Heroes", "Have no side" },
+                correctAnswer = 3,
+                Weight = 2
+            }
+        };
+
+        /// <summary>
+        /// Asks a question based on number
+        /// </summary>
+        /// <param name="qnum">The question number to ask</param>
+        private void askQuestion(int qnum)
+        {
+            try
+            {
+
+                if (qnum < 1 || qnum > quizQuestions.Count)
+                {
+                    //ivalid question
+                    return;
+                }
+
+                QuizQuestion question = quizQuestions[qnum - 1];
+
+                pictureBox1.Image = question.Image;
+                lblQuestion.Text = question.QuestionText;
+                button1.Text = question.Answers[0];
+                button2.Text = question.Answers[1];
+                button3.Text = question.Answers[2];
+                button4.Text = question.Answers[3];
+
+                correctAnswer = question.correctAnswer;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error ocurred while asking the question: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    
+            }
+
+        }
+    }
+    }
+
---
+++ QUIZ GAME/home.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace QUIZ_GAME
+{
+    partial class home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(181, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHOOSE A QUIZ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(428, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "AVENGERS QUIZ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Perpetua Titling MT", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(428, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "CARTOON NETWORK";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Ravie", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(428, 377);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "DISNEY";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::QUIZ_GAME.Properties.Resources.Disney__Gift_Subscription___Give_the_Gift_of_Disney__in_Canada;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 341);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(368, 97);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::QUIZ_GAME.Properties.Resources.Cartoon_Network_Logo_Animation;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 245);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(368, 90);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QUIZ_GAME.Properties.Resources.c0a4ebf7_f87c_42e7_95b7_e9b06615248f;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 137);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(368, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "home";
+            this.Text = "home";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
